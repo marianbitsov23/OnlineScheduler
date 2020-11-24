dddfa73eb3a3f74e807e05faf151af46470f7bc2
marianbitsov23 <marianbitsov23@gmail.com>
2020-11-24 10:11:57 +0200

    Fixed some buggs and loading pages in front end

---
+++ src/main/java/com/example/onlinescheduler/controller/PublicController.java
@@ -0,0 +1,29 @@
+package com.example.onlinescheduler.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("api/public")
+public class PublicController {
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public content.";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+    public String userAccess() {
+        return "User content.";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String adminAccess() {
+        return "Admin content.";
+    }
+}
--- src/main/java/com/example/onlinescheduler/security/services/UserDetailsImpl.java
+++ src/main/java/com/example/onlinescheduler/security/services/UserDetailsImpl.java
@@ -49,7 +49,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
