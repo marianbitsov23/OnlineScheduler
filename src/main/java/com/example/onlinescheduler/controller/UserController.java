a431908e2d35dbeea18516f64500f08453a047fa
marianbitsov23 <marianbitsov23@gmail.com>
2020-11-30 22:57:49 +0200

    User Update options

---
+++ src/main/java/com/example/onlinescheduler/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.example.onlinescheduler.controller;
+
+import com.example.onlinescheduler.model.User;
+import com.example.onlinescheduler.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@CrossOrigin(origins = "*", maxAge =  3600)
+@RestController
+@RequestMapping("/api/auth")
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/user/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        Optional<User> foundUser = userRepository.findById(id);
+
+        return foundUser.map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/user/{id}")
+    public ResponseEntity<User> updateUserInformation(@PathVariable Long id, @RequestBody User user) {
+        Optional<User> foundUser = userRepository.findById(id);
+
+        if (foundUser.isPresent()) {
+            User newUser = foundUser.get();
+            newUser.setUsername(user.getUsername());
+            newUser.setEmail(user.getEmail());
+            return new ResponseEntity<>(userRepository.save(newUser), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<HttpStatus> deleteUser(@PathVariable Long id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
