d8afdeef390d83bb3bc4c01f4e2f08e3dd80c641
marianbitsov23 <marianbitsov23@gmail.com>
2020-12-01 17:49:25 +0200

    Getting rid of some raw html code

--- react-project/src/components/board-user.component.jsx
+++ react-project/src/components/board-user.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import UserService from "../services/user.service";
+import { Container, Jumbotron } from "react-bootstrap";
 
 export default class BoardUser extends Component {
     constructor(props) {
@@ -33,11 +34,11 @@ export default class BoardUser extends Component {
 
     render() {
         return (
-            <div className="container">
-                <header className="jumbotron">
+            <Container>
+                <Jumbotron>
                     <h3>{this.state.content}</h3>
-                </header>
-            </div>
+                </Jumbotron>
+            </Container>
         );
     }
 }
